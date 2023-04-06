47433afcc9c70af747bf40f1fed996d65a2c4e79
itamar hess <Itamardarihess@gmail.com>
2023-04-07 01:07:30 +0300

    ui design update

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,30 +1,41 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import style from "../styles/navbar.module.css";
 
 const NavBar = () => {
     const [width, setWidth] = useState(window.innerWidth);
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+    const navbarRef = useRef(null);
 
     window.addEventListener("resize",()=>{
         setWidth(window.innerWidth);
     });
 
     function nav_btn_clicked(e){
-       nav_btn_animation(e);
+       open_close_navbar();
     }
 
-    function nav_btn_animation(e){
-        let btn = e.target;
-        btn.style.backgroundColor = "rgb(255,255,255,0.4)";
-        setTimeout(() => {
-            btn.style.backgroundColor = "transparent";
-        }, 300);
+    function open_close_navbar(){
+        if(isNavbarOpen === false){
+            navbarRef.current.style.opacity = "1";
+            setIsNavbarOpen(true);
+        }
+        if(isNavbarOpen === true){
+            navbarRef.current.style.opacity = "0";
+            setIsNavbarOpen(false);
+        }
     }
 
 
     return ( 
         <>
-        {width < 536 && <button className={style["nav-btn"]} onClick={nav_btn_clicked}>menue</button>}
-        <div className={style.navbar}>
+        {width < 536 && 
+        <div className={style["nav-btn"]} onClick={nav_btn_clicked}>
+            <div className={style.stick1}></div>
+            <div className={style.stick2}></div>
+            <div className={style.stick3}></div>
+        </div>
+        }
+        <div className={style.navbar} ref={navbarRef}>
             <div className={`${style.nav} ${style.home}`}>home</div>
             <div className={`${style.nav} ${style.about}`}>about</div>
             <div className={`${style.nav} ${style.contact}`}>contact</div>
