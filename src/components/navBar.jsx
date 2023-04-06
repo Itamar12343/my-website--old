e4b458a6bf48896c090bd35fd638588224a83c14
itamar hess <Itamardarihess@gmail.com>
2023-04-06 19:27:40 +0300

    update

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -7,9 +7,23 @@ const NavBar = () => {
     window.addEventListener("resize",()=>{
         setWidth(window.innerWidth);
     });
+
+    function nav_btn_clicked(e){
+       nav_btn_animation(e);
+    }
+
+    function nav_btn_animation(e){
+        let btn = e.target;
+        btn.style.backgroundColor = "rgb(255,255,255,0.4)";
+        setTimeout(() => {
+            btn.style.backgroundColor = "transparent";
+        }, 300);
+    }
+
+
     return ( 
         <>
-        {width < 536 && <button className={style["nav-btn"]}>menue</button>}
+        {width < 536 && <button className={style["nav-btn"]} onClick={nav_btn_clicked}>menue</button>}
         <div className={style.navbar}>
             <div className={`${style.nav} ${style.home}`}>home</div>
             <div className={`${style.nav} ${style.about}`}>about</div>
