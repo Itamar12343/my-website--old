077beba44b609afd95f12d06902e086877e1a01a
itamar hess <Itamardarihess@gmail.com>
2023-04-06 16:45:50 +0300

    navbar responsiveness

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import style from "../styles/navbar.module.css";
 
 const NavBar = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    window.addEventListener("resize",()=>{
+        setWidth(window.innerWidth);
+    });
     return ( 
+        <>
+        {width < 536 && <button className={style["nav-btn"]}>menue</button>}
         <div className={style.navbar}>
             <div className={`${style.nav} ${style.home}`}>home</div>
             <div className={`${style.nav} ${style.about}`}>about</div>
             <div className={`${style.nav} ${style.contact}`}>contact</div>
             <div className={`${style.nav} ${style.projects}`}>projects</div>
         </div>
+        </>
      );
 }
  
