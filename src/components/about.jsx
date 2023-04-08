3ed62bb90a2048ba2bfcb80aadf160b5d5e01949
itamar hess <Itamardarihess@gmail.com>
2023-04-08 20:44:33 +0300

    ui updates

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,9 +1,28 @@
+import { useRef } from "react";
 import style from "../styles/about.module.css";
 
 const About = () => {
+
+    const imgref = useRef(null);
+    const observer = new IntersectionObserver((entries)=>{
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+                console.log(entry.intersectionRatio);
+            }
+        });
+    },
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.6
+    });
+    setTimeout(() => {        
+        observer.observe(imgref.current);
+    }, 1000);
+
     return ( 
         <div className={style.box}>
-            <div className={style.img}></div>
+            <div className={style.img} ref={imgref}></div>
         </div>
      );
 }
