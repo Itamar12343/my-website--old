bb73d7ceb45e5e4b292624f82a18cfcf29c241e2
itamar hess <Itamardarihess@gmail.com>
2023-04-08 17:49:17 +0300

    about img

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,7 @@ import style from "../styles/about.module.css";
 const About = () => {
     return ( 
         <div className={style.box}>
-
+            <div className={style.img}></div>
         </div>
      );
 }
