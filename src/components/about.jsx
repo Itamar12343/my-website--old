a29d97c76f6a79a128fbd0637d323e10b81d7bc6
itamar hess <Itamardarihess@gmail.com>
2023-04-09 14:11:18 +0300

    ui design update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,6 +3,7 @@ import style from "../styles/about.module.css";
 
 const About = () => {
     const [imgref_animation, set_imgref_animation] = useState(false);
+    const text = "hey, my name is Itamar and I am 16 years old. I started when I was 14 and I learned wb development for 2 years. I learned by my own and made a lot of projects, recently I learned react and started doing a litle bit of backend and webSockets but I am more focused at frontend development. I love coding and I have been doing it for fun and I built most of the projects for friends and family"
 
     const imgref = useRef(null);
     const observer = new IntersectionObserver((entries)=>{
@@ -25,8 +26,8 @@ const About = () => {
 
     return ( 
         <div className={style.box}>
-            <div style={{transform: imgref_animation ? window.innerWidth > 764 ? "scale(1)" : "translate(-50%,0) scale(1)" : window.innerWidth > 764 ? "scale(0)" : "translate(-50%,0) scale(0)"}} className={style.img} ref={imgref}></div>
-            <p className={style.text}>text</p>
+            <p className={style.text}>{text}</p>
+            <div style={{transform: imgref_animation ? window.innerWidth > 938 ? "scale(1)" : "translate(-50%,0) scale(1)" : window.innerWidth > 938 ? "scale(0)" : "translate(-50%,0) scale(0)"}} className={style.img} ref={imgref}></div>
         </div>
      );
 }
