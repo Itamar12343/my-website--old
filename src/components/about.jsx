0b24ed57038cc7a82d88b0683d7696e2d71cdd16
itamar hess <Itamardarihess@gmail.com>
2023-04-12 22:36:24 +0300

    cleaner code and bug fixes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,35 +1,29 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import style from "../styles/about.module.css";
 
 const About = () => {
-    const [imgref_animation, set_imgref_animation] = useState(false);
     const text = "hey, my name is Itamar and I am 16 years old. I started when I was 14 and I learned wb development for 2 years. I am self tought and built a lot of projects, recently I learned react and started doing a litle bit of backend and webSockets but I am more focused at frontend development. I love coding and I have been doing it for fun and I built most of the projects for friends and family"
     const boxRef = useRef(null);
     const textElementRef = useRef(null);
 
     useEffect(()=>{
-        if(window.innerWidth < 938){
-        boxRef.current.style.height = `${textElementRef.current.offsetHeight + 580}px`;
-        }else{
-            boxRef.current.style.height = `${textElementRef.current.offsetHeight + 100}px`;
-        }
-    },[]);
+        matchBoxHeightToChildren();
+        startObservetion();
+    });
 
     window.addEventListener("resize",()=>{
-        if(window.innerWidth < 938){
-            boxRef.current.style.height = `${textElementRef.current.offsetHeight + 580}px`;
-            }else{
-                boxRef.current.style.height = `${textElementRef.current.offsetHeight + 100}px`;
-            }
+        matchBoxHeightToChildren();
     });
 
     const imgref = useRef(null);
     const observer = new IntersectionObserver((entries)=>{
+        const last = entries[0];
         entries.forEach(entry=>{
             if(entry.isIntersecting){
-                set_imgref_animation(true);
+                scrollAnimation(last.target);
+                observer.unobserve(last.target);
             }else{
-                set_imgref_animation(false);
+                endScrollAnimation(last.target);
             }
         });
     },
@@ -38,14 +32,53 @@ const About = () => {
       rootMargin: "0px",
       threshold: 0.6
     });
-    setTimeout(() => {        
-        observer.observe(imgref.current);
-    }, 1000);
+
+
+    function startObservetion(){
+        setTimeout(() => {        
+            observer.observe(imgref.current);
+            observer.observe(textElementRef.current);
+        }, 1000);
+    }
+
+    function matchBoxHeightToChildren(){
+        if(window.innerWidth < 938){
+            boxRef.current.style.height = `${textElementRef.current.offsetHeight + 580}px`;
+            }else{
+                boxRef.current.style.height = `${textElementRef.current.offsetHeight + 100}px`;
+            }
+    }
+
+    function scrollAnimation(element){
+        if(element.className.includes("img")){
+            if(window.innerWidth > 938){
+                element.style.transform = "scale(1)";
+            }else{
+                element.style.transform = "translate(-50%,0) scale(1)";
+            }
+
+        }else{
+            element.style.opacity = "1";
+        }
+    }
+
+    function endScrollAnimation(element){
+        if(element.className.includes("img")){
+            if(window.innerWidth > 938){
+                element.style.transform = "scale(0)";
+            }else{
+                element.style.transform = "translate(-50%,0) scale(0)";
+            }
+
+        }else{
+            element.style.opacity = "0";
+        }
+    }
 
     return ( 
         <div ref={boxRef} className={style.box}>
             <p ref={textElementRef} className={style.text}>{text}</p>
-            <div style={{transform: imgref_animation ? window.innerWidth > 938 ? "scale(1)" : "translate(-50%,0) scale(1)" : window.innerWidth > 938 ? "scale(0)" : "translate(-50%,0) scale(0)"}} className={style.img} ref={imgref}></div>
+            <div className={style.img} ref={imgref}></div>
         </div>
      );
 }
